adc8b8e644fdac1c47a2f1f4ac65ad093b2d37f5
Vadym <vadym.ivanchuk@scilifelab.se>
2024-04-14 20:01:11 +0200

    Keep showing overview

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,17 @@ function hideProjectOverview(id) {
   var overview = document.getElementById(id);
   overview.classList.remove("project-overview-visible");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var projectOverviews = document.querySelectorAll(".project-overview");
+
+  projectOverviews.forEach(function (projectOverview) {
+    projectOverview.addEventListener("mouseenter", function () {
+      showProjectOverview(projectOverview.id);
+    });
+
+    projectOverview.addEventListener("mouseleave", function () {
+      hideProjectOverview(projectOverview.id);
+    });
+  });
+});
