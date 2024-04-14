1b96cf2403df78071eb0569628f36ad93aa28bb9
Vadym <vadym.ivanchuk@scilifelab.se>
2024-04-14 19:56:02 +0200

    Simplify overview script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 function showProjectOverview(id) {
-  var element = document.getElementById(id);
-  element.classList.remove("roject-overview-hidden");
-  element.classList.add("project-overview-visible");
+  var overview = document.getElementById(id);
+  overview.classList.add("project-overview-visible");
 }
 
 function hideProjectOverview(id) {
-  var element = document.getElementById(id);
-  element.classList.remove("project-overview-visible");
-  element.classList.add("roject-overview-hidden");
+  var overview = document.getElementById(id);
+  overview.classList.remove("project-overview-visible");
 }
