3ff434dde2681a7554b41b7302a2d8122f475ca3
Vadym <vadym.ivanchuk@scilifelab.se>
2024-04-14 20:24:18 +0200

    Switch script to use event listeners & data targets

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,20 @@ function hideProjectOverview(id) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  var overviewButtons = document.querySelectorAll(".overview-button");
   var projectOverviews = document.querySelectorAll(".project-overview");
+
+  overviewButtons.forEach(function (overviewButton) {
+    overviewButton.addEventListener("mouseenter", function () {
+      var targetId = overviewButton.getAttribute("data-target");
+      showProjectOverview(targetId);
+    });
+
+    overviewButton.addEventListener("mouseleave", function () {
+      var targetId = overviewButton.getAttribute("data-target");
+      hideProjectOverview(targetId);
+    });
+  });
 
   projectOverviews.forEach(function (projectOverview) {
     projectOverview.addEventListener("mouseenter", function () {
